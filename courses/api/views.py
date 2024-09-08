6cb0227dffcc18ac0a4d52235a67003a5abededf
kodunaiff <kodunaiff@gmail.com>
2024-09-08 13:43:09 +0800

    added drf

---
+++ courses/api/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+from courses.models import Subject, Course
+from courses.api.serializers import SubjectSerializer
+from rest_framework.authentication import BasicAuthentication
+
+
+class SubjectListView(generics.ListAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+
+class SubjectDetailView(generics.RetrieveAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
--- educa/settings.py
+++ educa/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'embed_video',
     'debug_toolbar',
     'redisboard',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -169,3 +171,10 @@ CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+      'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}

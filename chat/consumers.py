eaa421429c38286d90b86e11225f95dc55de3604
kodunaiff <kodunaiff@gmail.com>
2024-09-10 11:56:42 +0800

    создал чат пока еще синхронный

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,18 +1,46 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from django.utils import timezone
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope['user']
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = f'chat_{self.id}'
+        # присоединиться к группе чат-комнаты
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
         # принять соединение
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # покинуть группу чат-комнаты
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
     # получить сообщение из WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        # отправить сообщение в WebSocket
-        self.send(text_data=json.dumps({'message': message}))
+        now = timezone.now()
+        # отправить сообщение в группу чат-комнаты
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'user': self.user.username,
+                'datetime': now.isoformat(),
+            }
+        )
+
+    # получить сообщение из группы чат-комнаты
+    def chat_message(self, event):
+        # отправить сообщение в веб-сокет
+        self.send(text_data=json.dumps(event))
